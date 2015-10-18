b355ff9c51d91951f2b45ba6cdc5c59869c7159c
Kai Carver <kaicarver@gmail.com>
2015-10-18 19:04:26 +0800

    worked ok for one TBBT episode

---
+++ subs.js
@@ -0,0 +1,379 @@
+var multiline = function(f) {
+  return f.toString().split('\n').slice(1, -1).join('\n');
+}
+// text from http://transcripts.foreverdreaming.org/viewtopic.php?f=159&t=22987
+var str = multiline(function(){/*  
+<p>What a wonderful day, thank you.</p>
+<p>Oh, we're glad you had fun.</p>
+<p>Blue Icees and a trip to The Container Store?</p>
+<p>It's like I died and went to the post-mortem, neuron-induced hallucination commonly mistaken as heaven.</p>
+<p>I still don't understand why you bought that pill caddie.</p>
+<p>You're a young man.</p>
+<p>Age is a state of mind, Leonard.</p>
+<p>In here I'm 90.</p>
+<p>Why are you taking your bins over there?</p>
+<p>That's just where I need them.</p>
+<p>You know, she doesn't have a lot of closet space.</p>
+<p>What's wrong with your closet?</p>
+<p>Uh...</p>
+<p>Well, honey, you know, now that Leonard and I are married, it kind of makes sense that we actually live together.</p>
+<p>So that's all this day was?</p>
+<p>A plan to butter me up before delivering bad news?</p>
+<p>Come on, buddy.</p>
+<p>No, I thought we were friends.</p>
+<p>You asked for a sip of my Icee.</p>
+<p>If you had your own straw, I might've said yes.</p>
+<p>Sheldon, please, we already feel bad about this.</p>
+<p>You know what they don't sell at The Container Store?</p>
+<p>Something large enough to contain my disappointment.</p>
+<p>Although, if anyone did, it would be them.</p>
+<p>♪ Our whole universe was in a hot, dense state ♪<br />
+♪ Then nearly 14 billion years ago expansion started... Wait! ♪<br />
+♪ The Earth began to cool ♪<br />
+♪ The autotrophs began to drool, Neanderthals developed tools ♪<br />
+♪ We built the Wall ♪<br />
+♪<em> We built the pyramids</em> ♪<br />
+♪ Math, Science, History, unraveling the mystery ♪<br />
+♪ That all started with a big bang ♪<br />
+♪<em> Bang!</em> ♪</p>
+<p>♪ The Big Bang Theory 9x04 ♪<br />
+Ihe 2003 Approximation<br />
+Original Air Date on October 12, 2015</p>
+<p>Thank you for letting me come speak with you.</p>
+<p>Of course.</p>
+<p>As my relationships with Penny and Amy are currently strained, I'm turning to you for female comfort and encouragement.</p>
+<p>Aww.</p>
+<p>I'm honored.</p>
+<p>I tried reaching out to my mother, but she was in Bible study.</p>
+<p>Leonard's mother is on a book tour.</p>
+<p>My Mee-Maw was taking a nap, and after a while Siri started repeating her answers.</p>
+<p>So, I'm your seventh choice.</p>
+<p>Yeah, I know, top ten, pretty exciting.</p>
+<p>How can I help you?</p>
+<p>Well, in addition to Amy leaving me, Leonard's moving in with Penny.</p>
+<p>It's difficult not to feel abandoned.</p>
+<p>Well, why don't you look at this as an opportunity?</p>
+<p>You had other roommates before Leonard.</p>
+<p>Maybe this is a chance to find someone new.</p>
+<p>Perhaps I could find someone better than Leonard.</p>
+<p>Someone I can rub in his face.</p>
+<p>Chris Pratt's all the rage right now.</p>
+<p>I wonder how he'd feel about taking the smaller bedroom.</p>
+<p>Hey.</p>
+<p>Hey.</p>
+<p>You know who would be the perfect roommate?</p>
+<p>Gandalf, but he's a smoker.</p>
+<p>Stuart.</p>
+<p>He's been living with us for a while now.</p>
+<p>I'm sure he'd love to get us out of his hair.</p>
+<p>Nope, couldn't be happier.</p>
+<p>Well, Sheldon's looking for a...</p>
+<p>Nope.</p>
+<p>He'll think about it.</p>
+<p>No I won't!</p>
+<p>Have you seen this Archie comic?</p>
+<p>It's actually <em>Archie versus Predator.</em></p>
+<p>How could Archie defeat Predator?</p>
+<p>I don't know.</p>
+<p>Maybe Jughead's a Terminator.</p>
+<p>Hey.</p>
+<p>Hey, Stuart.</p>
+<p>Do you guys know any musicians?</p>
+<p>Why?</p>
+<p>I was thinking it might be cool to have live music here a few nights a week.</p>
+<p>You know, give this place more of a &quot;staying in business&quot; vibe.</p>
+<p>What kind of music are you thinking of?</p>
+<p>I like all kinds of music, but my favorite genre is free.</p>
+<p>Hey, we've always talked about playing together.</p>
+<p>Well, it could be fun to try a little acoustic thing.</p>
+<p>Oh, we could play &quot;filk&quot; music.</p>
+<p>What's that?</p>
+<p>It's been around for years.</p>
+<p>It's like folk music, but with a sci-fi/fantasy theme.</p>
+<p>I like it.</p>
+<p>It sounds exactly like something I shouldn't be expected to pay for.</p>
+<p>Dude, if we do this, we're gonna need a cool band name.</p>
+<p>You know, I've actually (chuckles) had one I've been sitting on for years.</p>
+<p>Really?</p>
+<p>It was for this power trio I tried to put together in junior high, but I was short two friends.</p>
+<p>What is it?</p>
+<p>Footprints on the Moon.</p>
+<p>I just got chills.</p>
+<p>So did I.</p>
+<p>Me, too.</p>
+<p>But I-I might have Lyme Disease.</p>
+<p>Just a few more signatures, and we'll be finished.</p>
+<p>Initial here to acknowledge that you've returned your key.</p>
+<p>Okay.</p>
+<p>As my future neighbor, I'd like you to have a key.</p>
+<p>Initial here to acknowledge you received it.</p>
+<p>I'm proud of you. You're taking this really well.</p>
+<p>Well, it's not like I'm never going to see you two again.</p>
+<p>Which brings us to article 23 subsection C-- please check here to reserve your spot at the ten year roommate reunion.</p>
+<p>Do I really have to do that now?</p>
+<p>No, but if you want chicken and get stuck with the fish,year that's on you.</p>
+<p>All right.</p>
+<p>Oh, and lastly, please initial here to confirm that ownership of the living room couch is hereby transferred to me in perpetuity all throughout the universe and all alternate universes except for those universes where owning a couch is forbidden by the hive queen.</p>
+<p>In which case, all glory to the hive queen.</p>
+<p>All right, now all that's left is for us to sign and date the document, and we will officially no longer be roommates.</p>
+<p>What's the matter?</p>
+<p>It's harder than I thought.</p>
+<p>Let me help you.</p>
+<p>L-E-O-N...</p>
+<p>That helped.</p>
+<p>So I have an idea for a filk song.</p>
+<p>Kind of a, a power ballad, superhero crossover tale of epic proportions.</p>
+<p>Is it cold in here, 'cause, oh, my.</p>
+<p>Okay, so it's a, it's a David and Goliath story.</p>
+<p>It's about man against God.</p>
+<p>It's called &quot;Hammer and Whip: &quot;The Untold Story of Thor versus Indiana Jones.&quot;</p>
+<p>Thor versus Indiana Jones?</p>
+<p>You just blew my filking mind.</p>
+<p>Okay, I don't have it all worked out yet, but I was thinking something like, um...</p>
+<p>♪ Oh, Indy ♪<br />
+♪ Oh, Indy ♪<br />
+♪ The skies are so windy ♪<br />
+♪ Is that a flying man with a killer bod? ♪<br />
+♪ Wait ♪<br />
+♪ That's no man ♪<br />
+♪ It's a Norse god. ♪</p>
+<p>Oh yes, definitely.</p>
+<p>Hang on.</p>
+<p>♪ Thunder clapped as Thor raised his mighty hammer ♪<br />
+♪ Indy rapped, &quot;That's one bad mamma-jamma.&quot; ♪</p>
+<p>That-that is so good!</p>
+<p>Right?</p>
+<p>Mamma-jamma just came to me.</p>
+<p>Okay, and here's the hook.</p>
+<p>♪ Thor and Dr. Jones ♪<br />
+♪ Thor and Dr. Jones ♪<br />
+♪ One plays with lightning ♪<br />
+♪ The other plays with bones ♪</p>
+<p><strong>Both</strong>: ♪ Thor and Dr. Jones ♪</p>
+<p>(harmonizing): ♪ Thor and Dr. Jones ♪<br />
+♪ One plays with lightning ♪<br />
+♪ The other plays with bones. ♪</p>
+<p>Oh, man, that rocks.</p>
+<p>Hey, remember <em>The Ark of the Covenant?</em></p>
+<p>Yeah?</p>
+<p>That's how much we're gonna melt people's faces off.</p>
+<p>I appreciate your interest in the apartment.</p>
+<p>I just need to ask you a few standard questions.</p>
+<p>Sure.</p>
+<p>It says here you're a chemist.</p>
+<p>Which element on the periodic table do you feel is too big for its britches?</p>
+<p>Is that supposed to be a joke?</p>
+<p>Looks like argon's not the only one with an attitude problem.</p>
+<p>In general, would you say that you smell better, worse or the same as you do right now?</p>
+<p>I was going to ask you what is the best fruit, but then I realized what I want to ask you is why is there a Band-Aid on your forearm, but then I realized what I really want to ask you is just can you just go?</p>
+<p>You're healthy.</p>
+<p>You have a job in the sciences.</p>
+<p>I've got to say, if this credit report comes back good, you're the frontrunner.</p>
+<p>I'm not gonna be your roommate, Sheldon.</p>
+<p>But I met with 11 people, and they all walked out.</p>
+<p>And that Hollywood phony Chris Pratt never tweeted me back.</p>
+<p>I'm sure you'll find somebody else.</p>
+<p>I suppose.</p>
+<p>What happened to me, Amy?</p>
+<p>Years ago I was completely disengaged from my feelings.</p>
+<p>I'd say it was a happier time, but I was disengaged from my feelings, so who can tell?</p>
+<p>I don't know how to help you.</p>
+<p>You know, feelings are a part of life.</p>
+<p>They didn't used to be.</p>
+<p>You and Leonard and Penny, you all poisoned me with emotions.</p>
+<p>I was like the Tin Man, perfectly content until that evil Wizard gave him a heart.</p>
+<p>I don't think that was the point of the movie.</p>
+<p>Fine, then I was like Pinocchio before that jerk Geppetto went and made him a real boy.</p>
+<p>There you go.</p>
+<p>So, what are you thinking for dinner?</p>
+<p>Well, it's Thai food night.</p>
+<p>Well, honey, you don't live with Sheldon anymore.</p>
+<p>You can have anything you want.</p>
+<p>You're right.</p>
+<p>But what?</p>
+<p>Mexican?</p>
+<p>Italian? German?</p>
+<p>Indian? Greek?</p>
+<p>Cuban? Chinese?</p>
+<p>Pizza? Barbecue?</p>
+<p>Korean? Korean barbecue?</p>
+<p>How about Thai food?</p>
+<p>Oh, thank God. Hey, we're going to dinner. You want to come?</p>
+<p>Oh, I wish I could, but I realized I've become too emotionally vulnerable, so, like an operating system, I'm restoring my life to the last stable version, which was in 2003, the day before I met Leonard.</p>
+<p>You heard him, no.</p>
+<p>Hang on.</p>
+<p>Wait, you actually think it's 2003?</p>
+<p>No, just because I'm living my life like it was 12 years ago doesn't mean I'm delusional.</p>
+<p>And since it is 2003, I don't know who you are, so please exit the premises before I call the police on my stylish new flip phone.</p>
+<p>Are you doing okay?</p>
+<p>I guess.</p>
+<p>I'm just, you know, worried about Sheldon.</p>
+<p>Well, come on, he's a grown man in his 30s pretending to be a grown man in his 20s.</p>
+<p>He's fine.</p>
+<p>I hope so.</p>
+<p>What else can you do? Move back in with him?</p>
+<p>No, of course not. I-I just feel bad.</p>
+<p>Well, so do I, but don't you want to live with your wife and set the thermostat to whatever you want and have your body tell you when it's time to go to the bathroom, you know, not a schedule slipped underneath your door every morning?</p>
+<p>I did like that he had the weather on it.</p>
+<p>Trust me, this is the right thing.</p>
+<p>I know.</p>
+<p>And it's not like we're abandoning him.</p>
+<p>Plus, we can FaceTime him whenever we want, you know, once iPhones are invented in his universe.</p>
+<p>I can't believe Sheldon asked you to be his roommate.</p>
+<p>I can't believe he ran my credit.</p>
+<p>Hey, if you're open to living with someone great, I'll give you $1,000 to take Stuart.</p>
+<p>You really should've gone on the Internet and checked how long that kind of thing lives before you got one.</p>
+<p>I'm sorry.</p>
+<p>I just thought you might have a pasty, weirdo-shaped hole in your life.</p>
+<p>Hey, I know Sheldon's quirky, but he's also brilliant and insightful.</p>
+<p>I think calling him a weirdo is a little unfair.</p>
+<p>Hello, 2003!</p>
+<p>Hey, we brought you Thai.</p>
+<p>Where is everything?</p>
+<p>In my present, it's in the future.</p>
+<p>In your present, it's been crammed in the bedroom by an enterprising young man I met in The Home Depot parking lot.</p>
+<p>I know what you're doing.</p>
+<p>You're trying to get attention so we'll feel bad for you, but it's not happening.</p>
+<p>No, what I'm doing is trying to figure out how to live my life now that everyone is leaving me.</p>
+<p>Will you knock it off?</p>
+<p>We're across the hall.</p>
+<p>As the kids are saying today, &quot;talk to the hand.&quot;</p>
+<p>They're not saying that.</p>
+<p>They are in 2003.</p>
+<p>No, they're really not.</p>
+<p>♪ Thor and Dr. Jones ♪<br />
+♪ Thor and Dr. Jones ♪<br />
+♪ One runs from Loki ♪<br />
+♪ The other runs from stones. ♪</p>
+<p>So, what do you think?</p>
+<p>Wow.</p>
+<p>Runs from stones means that-that big boulder, but I couldn't rhyme anything with boulder.</p>
+<p>Colder, shoulder, soldier, folder...</p>
+<p>Right, yeah, right.</p>
+<p>But... what do you think?</p>
+<p>I think it's very cute.</p>
+<p>Cute? It's not cute.</p>
+<p>Cute is children dressed as vegetables.</p>
+<p>Okay, fine, it's not cute.</p>
+<p>Just be honest. You don't like it.</p>
+<p>I didn't say that.</p>
+<p>I-I just like music you can dance to.</p>
+<p>You can dance to this!</p>
+<p>Um, uh, uh...</p>
+<p>♪ Thor and Dr. Jones ♪<br />
+♪ Thor and Dr. Jones ♪<br />
+♪ One plays with lightning ♪<br />
+♪ The other plays with bones. ♪</p>
+<p>I'm telling you, dude, the song has no groove.</p>
+<p>You can't dance to it.</p>
+<p>Who cares?</p>
+<p>I thought the whole point of Footprints on the Moon was to write songs that make people think.</p>
+<p>You can do both, like Michael Jackson's &quot;Billie Jean.&quot;</p>
+<p>While you're dancing you're thinking, like, &quot;Darn it, whose baby is it?&quot;</p>
+<p>Where's this coming from?</p>
+<p>A few hours ago, we both loved this song.</p>
+<p>I still love it.</p>
+<p>I just think there's room for improvement.</p>
+<p>You played it for Emily, didn't you?</p>
+<p>Yeah, so what? You are such a wimp.</p>
+<p>She didn't like it, now you don't like it.</p>
+<p>No, no. I'm just evolving as a musician.</p>
+<p>You're the one who's stuck in the past.</p>
+<p>The past was lunch!</p>
+<p>You know what's really happening here?</p>
+<p>Your girlfriend is breaking up our band.</p>
+<p>She has nothing to do with this.</p>
+<p>I am my own man.</p>
+<p>Oh, please. Your brain belongs to whoever's willing to sleep with you.</p>
+<p>That is so not true.</p>
+<p>Really?</p>
+<p>Remember when you were gonna get circumcised for Rachel Bernstein?</p>
+<p>That had nothing to do with Rachel.</p>
+<p>It was an overreaction to a bad zipper injury.</p>
+<p>I don't need this.</p>
+<p>I'm quitting the band!</p>
+<p>Fine, I quit, too!</p>
+<p>Then get out of my house!</p>
+<p>With pleasure!</p>
+<p>(door slams)</p>
+<p>(sighs)</p>
+<p>Raj, wait.</p>
+<p>What took you so long?</p>
+<p><strong>Leonard</strong>: Buddy, I know me moving in with Penny feels like a big change, but it's not.</p>
+<p>How can you say that?</p>
+<p>Amy's gone, and you two are married now, so it's only a matter of time before you're gone, too.</p>
+<p>Okay, you don't know what's gonna happen.</p>
+<p>No, I do.</p>
+<p>Eventually you'll want more space, and you'll move into a house, and then instead of dinner a couple of times a week, it'll only be a couple of times a month, and then it'll only be on special occasions, like when Bernadette divorces Wolowitz.</p>
+<p>Or-or-or like when Koothrappali's weird girlfriend admits where she buried his body.</p>
+<p>Or Amy's wedding, where she's marrying someone better than me.</p>
+<p>Okay, look, we don't need to rush into anything.</p>
+<p>All right?</p>
+<p>Maybe instead of Leonard moving in with me, we just leave things the way they are, and... sometimes we'll sleep over there, and sometimes we'll sleep over here.</p>
+<p>But mostly here.</p>
+<p>Sure.</p>
+<p>W-W-What about what you said in the restaurant?</p>
+<p>Well, it's not forever.</p>
+<p>It's just for a while.</p>
+<p>If you want, we can think of him like he's our dog.</p>
+<p>You can.</p>
+<p>I'm happy when you come home.</p>
+<p>And I'm scared of fireworks.</p>
+<p>And by the way, on July Fourth, we're all sleeping here.</p>
+<p>Fine.</p>
+<p>All right, great!</p>
+<p>Just give me one minute, and I'll get started on a new Roommate Agreement.</p>
+<p>Yeah.</p>
+<p>Nothing from Pratt. We're good.</p>
+<p>♪ Indy' whip snapped ♪<br />
+♪ Thor's hammer missed ♪<br />
+♪ It was Avenger ♪<br />
+♪ Versus archeologist ♪<br />
+♪ Indy held his ground ♪<br />
+♪ And straightened his fedora ♪<br />
+♪ Thor said, &quot;That's a nice look ♪<br />
+♪ In 1944-a&quot; ♪</p>
+<p><strong>Both</strong>: ♪ Thor and Dr. Jones ♪<br />
+♪ Thor and Dr. Jones ♪<br />
+♪ One plays with lightning ♪<br />
+♪ The other plays bones ♪<br />
+♪ Thor and Dr. Jones ♪</p>
+<p>(high-pitched): ♪ Thor and Dr. Jones ♪<br />
+♪ Thor and Dr. Jones ♪<br />
+♪ One plays with lightning ♪<br />
+♪ The other plays bones. ♪</p>
+<p>(song ends)</p>
+<p>Play something we can dance to.</p>
+*/});
+var lines = str.split("\n");
+var box = document.createElement('div');
+box.id="box";
+box.style.position = 'fixed';
+box.style.backgroundColor = 'pink';
+box.style.color = 'black';
+box.style.textAlign = 'center';
+box.style.top = 0;
+var curLine = 0;
+document.body.appendChild(box);
+nextLine(0);
+document.onkeydown = function() {
+  console.log(window.event.keyCode);
+  switch (window.event.keyCode) {
+  case 37: nextLine(-1); return false;; // ← Left key
+  case 38: nextLine(-1); return false;; // ↑ Up key
+  case 13: nextLine(0);  return false;; // ↵ Enter key
+  case 39: nextLine(1);  return false;; // → Right key
+  case 40: nextLine(1);  return false;; // ↓ Down key
+  }
+  return true;
+}
+function nextLine(pos) {
+  var box = document.getElementById("box");
+  curLine += pos;
+  var line = lines[curLine].replace(/<.*?>/g, '');
+  box.innerHTML = line;
+}
+
+/* Insert into any page using bookmarklet like this:
+javascript:(function(s){s.src='http://127.0.0.1:8887/cygwin64/home/kai/github/quicksubs/subs.js?'+Math.random();document.body.appendChild(s)})(document.createElement('script'))
+*/
